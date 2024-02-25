40ef37e30294082c295017081543cbde93b4e35f
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-02-25 01:38:03 +0100

    Idk man

--- src/cleanup.py
+++ src/cleanup.py
@@ -1,7 +1,9 @@
 import os
 import glob
+
 def remove_images(directory_path):
     files = glob.glob(os.path.join(directory_path, '*'))
     [os.remove(file) for file in files if os.path.isfile(file)]
+
 image_directory = "src/static/generated-images/"
 remove_images(image_directory)
