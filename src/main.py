37689b9815f0ef7515aad4712d8326e045e4636f
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-03-09 12:28:16 +0100

    Enable image compression, Minor Changes

--- src/main.py
+++ src/main.py
@@ -1,29 +1,29 @@
-import os
+from os import path, makedirs
 from uuid import uuid4
 from PIL import Image
 from constants import special_characters
 
 def generate_filename():
-    unique_id = uuid4()
+    unique_id = uuid4().hex
     return f"{unique_id}.png"
 
 def get_font_paths(font, color):
-    base_path = os.path.join('src', 'static', 'assets', 'fonts', f'font-{font}', f'font-{font}-{color}')
-    return [os.path.join(base_path, folder) for folder in ['letters', 'numbers', 'symbols']]
+    base_path = path.join('src', 'static', 'assets', 'fonts', f'font-{font}', f'font-{font}-{color}')
+    return [path.join(base_path, folder) for folder in ('letters', 'numbers', 'symbols')]
 
 def get_character_image_path(character, font_paths):
-    CHARACTERS_FOLDER, NUMBERS_FOLDER, SYMBOLS_FOLDER = font_paths
+    characters_folder, numbers_folder, symbols_folder = font_paths
 
     if character.isspace():
         return None
     elif character.islower():
-        character_image_path = os.path.join(CHARACTERS_FOLDER, 'lower-case', f"{character}.png")
+        character_image_path = path.join(characters_folder, 'lower-case', f"{character}.png")
     elif character.isupper():
-        character_image_path = os.path.join(CHARACTERS_FOLDER, 'upper-case', f"{character}.png")
+        character_image_path = path.join(characters_folder, 'upper-case', f"{character}.png")
     elif character.isdigit():
-        character_image_path = os.path.join(NUMBERS_FOLDER, f"{character}.png")
+        character_image_path = path.join(numbers_folder, f"{character}.png")
     else:
-        character_image_path = os.path.join(SYMBOLS_FOLDER, f"{special_characters.get(character, '')}.png")
+        character_image_path = path.join(symbols_folder, f"{special_characters.get(character, '')}.png")
 
     return character_image_path
 
@@ -32,7 +32,7 @@ def load_character_image(character, font_paths):
         return Image.new("RGBA", (30, 1), (0, 0, 0, 0))
 
     character_image_path = get_character_image_path(character, font_paths)
-    if character_image_path is None or not os.path.isfile(character_image_path):
+    if not character_image_path or not path.isfile(character_image_path):
         raise FileNotFoundError(f"Unsupported character '{character}',")
 
     return Image.open(character_image_path)
@@ -52,11 +52,11 @@ def generate_image(text, filename, font_paths):
         final_image.paste(character_image, (x_position, y_position))
         x_position += character_image.width
 
-    image_directory = os.path.join('src', 'static', 'generated-images')
-    os.makedirs(image_directory, exist_ok=True)
+    image_directory = path.join('src', 'static', 'generated-images')
+    makedirs(image_directory, exist_ok=True)
 
-    image_path = os.path.join(image_directory, filename)
-    final_image.save(image_path)
+    image_path = path.join(image_directory, filename)
+    final_image.save(image_path, optimize=True)
 
     image_url = f'static/generated-images/{filename}'
 
